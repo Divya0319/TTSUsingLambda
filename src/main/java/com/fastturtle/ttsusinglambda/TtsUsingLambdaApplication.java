4010bc26a88a9ce4eadbf13fb2e30385f7d6d00d
divya0319 <divygupta0319@gmail.com>
2025-01-28 13:54:56 +0530

    Initial commit with necessary dependencies

---
+++ src/main/java/com/fastturtle/ttsusinglambda/TtsUsingLambdaApplication.java
@@ -0,0 +1,13 @@
+package com.fastturtle.ttsusinglambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtsUsingLambdaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtsUsingLambdaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fastturtle/ttsusinglambda/TtsUsingLambdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastturtle.ttsusinglambda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtsUsingLambdaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
